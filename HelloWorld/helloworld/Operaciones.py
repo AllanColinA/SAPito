29c56f0924df8a251feda8d73a89993bca286ba6
ithza <ithza@debian.ithza>
2015-12-14 20:24:45 -0600

    Ya hice una clase operaciones y tu estas de borracho (:

---
+++ HelloWorld/helloworld/Operaciones.py
@@ -0,0 +1,20 @@
+# To change this license header, choose License Headers in Project Properties.
+# To change this template file, choose Tools | Templates
+# and open the template in the editor.
+
+def operaciones(a,b):
+    suma = a + b
+    print suma
+    
+    resta = a - b
+    print resta
+    
+    multiplicacion = a * b
+    print multiplicacion
+    
+    potencia = a ** b
+    print potencia
+    
+operaciones(4, 5)
+    
+    
