56f7e6ba1792314fdd4ec0aa0ce0cbb134138e33
ithza <ithza@debian.ithza>
2015-12-14 20:13:51 -0600

    "hello world"

---
+++ HelloWorld/helloworld/helloworld.py
@@ -0,0 +1,11 @@
+# To change this license header, choose License Headers in Project Properties.
+# To change this template file, choose Tools | Templates
+# and open the template in the editor.
+
+if __name__ == "__main__":
+    print("Hello World")
+def fun():
+    suma = 3 + 3
+    print "Esta es la suma" + str(suma)
+fun()
+    
